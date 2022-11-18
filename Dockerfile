edc089ff09d659def06f1bd1b01c96ade6da7856
VVlVolodin <skr1n1995@mail.ru>
2022-11-18 11:33:09 +0300

    add LogAspect and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM openjdk:11
+
+#ARG JAR_FILE=target/*.jar
+#RUN mkdir -p /usr/src/app
+#RUN addgroup -S spring && adduser -S spring -G spring
+#USER spring:spring
+#WORKDIR /usr/src/app
+
+COPY target/spring-sport-1.0-SNAPSHOT.jar /spring-sport.jar
+
+ENTRYPOINT ["java","-jar","/spring-sport.jar"]
--- src/main/java/org/sport/foot/aspect/LogAspect.java
+++ src/main/java/org/sport/foot/aspect/LogAspect.java
@@ -1,17 +1,27 @@
 package org.sport.foot.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 
 
-
-//todo сделать логирование названия метода в который вошли и вышли
 @Aspect
+@Configuration
 public class LogAspect {
 
-    @Before("execution()/greeting()")
-    public void beforeGetFindById() {
+    private static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
 
+    @Before(value = "execution(* org.sport.foot.controller.*.*(..))")
+    public void logStatementBefore(JoinPoint joinPoint) {
+        LOG.info("Executing... {}", joinPoint);
     }
 
+    @After(value = "execution(* org.sport.foot.controller.*.*(..))")
+    public void logStatementAfter(JoinPoint joinPoint) {
+        LOG.info("Complete execution {}", joinPoint);
+    }
 }
